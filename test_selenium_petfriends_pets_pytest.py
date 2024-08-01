95494325d988a7f181f282b5e039601fcfbca873
Lana1199 <161425027+Lana1199@users.noreply.github.com>
2024-08-02 00:22:11 +0300

    Update test_selenium_petfriends_pets_pytest.py

--- test_selenium_petfriends_pets_pytest.py
+++ test_selenium_petfriends_pets_pytest.py
@@ -1,11 +1,8 @@
 import pytest
-import chromedriver_autoinstaller
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait as wdw
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(autouse=True)
 def driver():
@@ -16,41 +13,43 @@ def driver():
     yield driver
     driver.quit()
 
-#Неявные ожидания. явные ожидания
+#Неявные ожидания.
 
 def test_all_pets_cards(driver):
-    wdw(driver,5).until(EC.presence_of_element_located((By.ID, 'email')))
    #Вводим email
     driver.find_element(By.ID, 'email').send_keys('sv.lana1199@gmail.com')
    #Вводим пароль
     driver.find_element(By.ID, 'pass').send_keys('dveri2014')
     #Ставим величину неявного ожидания элементов в 5 секунд.
-    #driver.implicitly_wait(5)
+    driver.implicitly_wait(5)
    #Нажимаем на кнопку входа в аккаунт
     driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
    #Проверяем, что мы оказались на главной странице сайта
     assert driver.find_element(By.TAG_NAME, 'h1').text == "PetFriends"
+
+
     #Нажимаем на кнопку Мои питомцы.
     driver.find_element(By.XPATH, '//a[text()="Мои питомцы"]').click()
+
    # Проверяем, что мы оказались на главной странице пользователя
     assert driver.find_element(By.TAG_NAME, 'H2').text == "Лана"
 
    # Объявили три переменные, в которых записали все найденные элементы на странице: в images — все картинки питомцев,
    # в names — все их имена, в ages — возрасты.
 
-    #driver.implicitly_wait(1)
+    driver.implicitly_wait(2)
 
     images = driver.find_elements(By.CSS_SELECTOR, '.card-deck .card-img-top')
-   #
-   #  #driver.implicitly_wait(1)
-   #
+
+    driver.implicitly_wait(2)
+
     names = driver.find_elements(By.CSS_SELECTOR, '.card-deck .card-title')
-   #
-   #  #driver.implicitly_wait(1)
-   #
+
+    driver.implicitly_wait(2)
+
     ages = driver.find_elements(By.CSS_SELECTOR, '.card-deck .age')
-   #
-   # #Организуем цикл, который может перебрать все эти элементы.
+
+   #Организуем цикл, который может перебрать все эти элементы.
 
     for i in range(len(names)):
         assert images[i].get_attribute('src') != ''
@@ -60,35 +59,33 @@ def test_all_pets_cards(driver):
 
 #Явные ожидания.
 
-# def test_pets_table(driver):
-#     """Проверка таблицы питомцев на наличие всех колонок"""
-#
-#     driver.find_element(By.ID, 'email').send_keys('sv.lana1199@gmail.com')
-#     # Вводим пароль
-#     driver.find_element(By.ID, 'pass').send_keys('dveri2014')
-#     # Нажимаем на кнопку входа в аккаунт
-#     driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
-#     # Ожидание загрузки главной страницы
-#     WebDriverWait(driver, 10).until(EC.visibility_of_element_located(By.XPATH, '//a[text()=\"Мои питомцы\"]'))
-#
-#     # Нажимаем на кнопку Мои питомцы.
-#     driver.find_element(By.XPATH, '//a[text()="Мои питомцы"]').click()
-#
-#
-#
-#     # Переход на страницу со списком питомцев
-#     driver.get('https://petfriends.skillfactory.ru/my_pets')
-#
-#     # Ожидание появления таблицы
-#     WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.table')))
-#
-#     # Проверка наличия всех колонок в таблице
-#     expected_columns = ['Фото', 'Имя', 'Порода', 'Возраст','']
-#     table_head = driver.find_element(By.CSS_SELECTOR, '.table thead tr')
-#     columns = table_head.find_elements(By.TAG_NAME, 'th')
-#     column_names = [column.text for column in columns]
-#     assert column_names == expected_columns
+def test_pets_table(driver):
+    """Проверка таблицы питомцев на наличие всех колонок"""
+
+    driver.find_element(By.ID, 'email').send_keys('sv.lana1199@gmail.com')
+    # Вводим пароль
+    driver.find_element(By.ID, 'pass').send_keys('dveri2014')
+    # Нажимаем на кнопку входа в аккаунт
+    driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+    # Ожидание загрузки главной страницы
+    wdw(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//a[text()=\"Мои питомцы\"]')))
+
+    # Нажимаем на кнопку Мои питомцы.
+    driver.find_element(By.XPATH, '//a[text()="Мои питомцы"]').click()
 
 
+
+    # Переход на страницу со списком питомцев
+    driver.get('https://petfriends.skillfactory.ru/my_pets')
+
+    # Ожидание появления таблицы
+    wdw(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.table')))
+
+    # Проверка наличия всех колонок в таблице
+    expected_columns = ['Фото', 'Имя', 'Порода', 'Возраст','']
+    table_head = driver.find_element(By.CSS_SELECTOR, '.table thead tr')
+    columns = table_head.find_elements(By.TAG_NAME, 'th')
+    column_names = [column.text for column in columns]
+    assert column_names == expected_columns
 
 
